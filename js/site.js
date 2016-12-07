ade083251b652bcdb7810b6b103e47e82238686c
kli-z <klimazachary@gmail.com>
2016-12-07 10:56:09 -0600

    Created a get request. URL properly concatenates but the request does not fire.

--- js/site.js
+++ js/site.js
@@ -13,16 +13,31 @@ $.noConflict(); //reserve $ namespace
       $("#formpetfinder").on("submit", function(event) {
         event.preventDefault(); //prevents submit from reloading page and dumping stuff into the URL
         var specifics = [$('#searchlocation').val(), $("#searchanimal").val(), $("#searchbreed").val()];
-        queryURL = "http://api.petfinder.com/my.method?key=" + APIkey;
+        //note the single quotes below for the get request
+        queryURL = "'http://api.petfinder.com/pet.find?key=" + APIkey +
+          "&location=" + specifics[0] + "&animal=" + specifics[1] + "&breed=" + specifics[2] +
+          "&count=2&output=full&format=json'";
+
+        alert("Form submitted.\nqueryURL: " + queryURL + "\n\nSending query now.");
+
         response = function() {
-          $.ajax ({
-            type: "GET", url: queryURL
-          });
+          $.get(
+            queryURL,
+            function(data) {
+              alert("BOX!");
+            }
+          );
         }
+        alert("Query sent.");
       });
     });
   }
 )(jQuery); //pass "jQuery" in for $ instead of assuming
+
+
+
+
+
 
 
 
