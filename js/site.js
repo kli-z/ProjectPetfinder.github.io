464f8375e8b3951a10323e2905a0b7b61de88a5c
kli-z <klimazachary@gmail.com>
2016-12-06 20:27:42 -0600

    Began work on querying the API, not functional yet. Switched to double quotes. Created a place for commented-out code instead of leaving it to interrupt flow.

--- js/site.js
+++ js/site.js
@@ -1,22 +1,36 @@
 //site.js
 //Javascript file for ProjectPetfinder that queries the Petfinder database.
 
+alert("Compiled");
+
 $.noConflict(); //reserve $ namespace
 (
   function($) {
     $(document).ready(function() {
-      $('#formpetfinder').on('submit', function(event) {
 
-        /*if $('#searchlocation').val() === "") { //if zipcode field is blank
-          //$('label').append('<b>Required</b>');
+      var APIkey = "dbc84ba782ca38e121f2d7c21d0b2787";
+
+      $("#formpetfinder").on("submit", function(event) {
+        event.preventDefault(); //prevents submit from reloading page and dumping stuff into the URL
+        var specifics = [$('#searchlocation').val(), $("#searchanimal").val(), $("#searchbreed").val()];
+        queryURL = "http://api.petfinder.com/my.method?key=" + APIkey;
+        response = function() {
+          $.ajax ({
+            type: "GET", url: queryURL
+          
         }
-        else {
-          //$('label').append('<b>Accepted</b>');
-        }*/
-
-        event.preventDefault();
       });
     });
   }
 )(jQuery); //pass "jQuery" in for $ instead of assuming
 
+
+
+/* Commented-out */
+
+/*if $('#searchlocation').val() === "") { //if zipcode field is blank
+          //$('label').append('<b>Required</b>');
+        }
+        else {
+          //$('label').append('<b>Accepted</b>');
+        }*/
